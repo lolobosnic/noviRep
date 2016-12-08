8cae50e7d39c06624a479250a2152c3dce8e9cf4
Lovro Bosnic <igranakvadrat@net.hr>
2016-12-08 22:43:33 +0100

    prvi

---
+++ skripta.js
@@ -0,0 +1,15 @@
+var appModule = angular.module("app1",[]);
+
+appModule.controller("homeController", function($scope)
+{
+    
+    $scope.ime = "";
+    
+    
+    $scope.varijabla = "neki Text Za Prikazati U Viewu"
+    
+    
+    $scope.isClicked =false;
+    
+    
+});
